93010567a2744d8e6b57cd9ff7bf7f7851cf751c
Adinarayan Sahu <adinarayansahu7@gmail.com>
2025-07-30 09:22:16 +0530

    Add reverse character array solution

---
+++ arrays/ReverseCharacters.java
@@ -0,0 +1,8 @@
+class ReverseCharacters{
+    public static void main(String[] args) {
+        char[] arr = {'A', 'B', 'C', 'D', 'E'};
+        for(int i = arr.length - 1; i >= 0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
