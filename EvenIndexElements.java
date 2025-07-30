98eda833ca4922f6d39de72dd8a1ac9139548b96
Adinarayan Sahu <adinarayansahu7@gmail.com>
2025-07-30 10:14:31 +0530

    Fix: Corrected EvenIndexElements.java logic

--- EvenIndexElements.java
+++ EvenIndexElements.java
@@ -1,9 +1,9 @@
 public class EvenIndexElements {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6};
-        for (int i : arr){
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        for(int i = 0; i < arr.length; i++){
             if (i%2==0) {
-                System.out.println(i);
+                System.out.println(arr[i]);
             }
         }
     }
