407dc368f8cbb33b2dc364f9cf48875f39b0a58c
Adinarayan Sahu <adinarayansahu7@gmail.com>
2025-07-30 10:27:52 +0530

    Add odd elements print logic & format all files

--- arrays/EvenIndexElements.java
+++ arrays/EvenIndexElements.java
@@ -1,8 +1,8 @@
 public class EvenIndexElements {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        for(int i = 0; i < arr.length; i++){
-            if (i%2==0) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
                 System.out.println(arr[i]);
             }
         }
--- arrays/OddElements.java
+++ arrays/OddElements.java
@@ -1,11 +1,10 @@
-public class EvenElements {
+public class OddElements {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
         for (int i : arr) {
-            if (i % 2 == 0) {
+            if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
     }
-
 }
